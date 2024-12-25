885c5fb5eb932bebdc9468027d95624f353773aa
Prithvi Raj <prithvi07raj07@gmail.com>
2024-12-26 01:39:26 +0530

    use some more routes

--- Server making/server.js
+++ Server making/server.js
@@ -1,5 +1,11 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
+    if(req.url==='/secretdata'){
+        res.end('Their is no seceret data');
+    }
+    if(req.url==='/'){
+        res.end('Welcome to our homepage');
+    }
     res.end('Hello World');
 })
 
