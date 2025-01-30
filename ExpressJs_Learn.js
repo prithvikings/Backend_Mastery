7f432acad0c753d477308dea581c2e28ac90a34c
Prithvi Raj <prithvi07raj07@gmail.com>
2025-01-31 01:26:32 +0530

    Add introductory content for HTTP protocol and Express.js framework

---
+++ ExpressJs_Learn.js
@@ -0,0 +1,34 @@
+/*
+Express.js Framework
+Introduction to Express.js
+Setting up Express.js application
+Routing in Express.js
+middleware in Express.js
+request and response in Express.js
+error handling in Express.js
+
+-Express.js is a web application framework for Node.js. -It is designed for building web applications and APIs.
+-It has been called the de facto standard server framework for Node.js.
+-Express.js simplifies the development process of web applications and APIs.
+-Express.js is a minimal and flexible Node.js web application framework that provides a robust set of features to develop web and mobile applications.
+-It facilitates the rapid development of Node based Web applications.
+-Following are some of the core features of Express framework −
+-Allows to set up middlewares to respond to HTTP Requests.
+-Defines a routing table which is used to perform different actions based on HTTP Method and URL.
+-Allows to dynamically render HTML Pages based on passing arguments to templates.
+-It has inbuilt support for HTTP/2 and HTTP/3.
+-Allows to define error handling middlewares.
+-Express js manages everything from receiving requests to sending responses.
+
+
+difference between Express.js and Node.js
+-Node.js is a platform to execute JavaScript code server-side.
+-Express.js is a framework built on top of Node.js to create web applications and APIs.
+
+difference between framework and library
+
+-A library is a collection of functions that you can call, these functions will not run unless you call them.
+
+-A framework is a collection of patterns and libraries to help with building an application. The framework will call your code when it needs something specific.
+
+*/
--- Server making/server.js
+++ Server making/server.js
@@ -1,3 +1,19 @@
+//http- protocol 
+//yahi protocol hai ya rule hai jisko follow kare bina aap internet pe naa hi kuch bhej sakte ho, naa hi kuchh manga sakte ho
+
+
+//what is method in programming language
+//method is a function which is used to perform some operation on the data or to perform some task.
+
+//http module is used to create server in node.js
+
+//createServer method is used to create server
+//createServer method takes a callback function as an argument which takes two arguments request and response
+//request object is used to get the information about the request made by the client and response object is used to send the response to the client 
+//listen method is used to start the server and it takes two arguments port number and a callback function
+//callback function is used to print a message on the console that the server is running on a particular port number
+
+
 const http=require('http');
 const server=http.createServer((req,res)=>{
     if(req.url==='/secretdata'){
