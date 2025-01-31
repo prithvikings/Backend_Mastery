6020f6ecf8d8e2ef1a684895cc67d00e3b8f95b2
Prithvi Raj <prithvi07raj07@gmail.com>
2025-02-01 02:53:37 +0530

    Initialize authentication project: add package.json, app.js, and .gitignore for dependencies and setup

---
+++ Authentication_Learn/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+app.use(cookieParser());
+
+// for setting cookie using jasonwebtoken
+app.get('/',(req,res)=>{
+    const token = jwt.sign({name:"Aman"},'my_secret_key');
+    res.cookie('token',token).send('cookie set');
+    console.log("token is set : ",token);
+})
+
+
+// for reading cookie
+app.get('/read',(req,res)=>{
+    res.send('checking on other page that cookie is set or not');
+    console.log("cookies : ",req.cookies.token);
+})
+
+// for clearing cookie
+app.get('/clear',(req,res)=>{
+    res.clearCookie('name').send('cookie cleared');
+})
+
+
+var hashed="";
+// for hashing password
+app.get("/auth",(req,res)=>{
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err) throw err;
+        bcrypt.hash('password',salt,(err,hash)=>{
+            if(err) throw err;
+            res.send(hash);
+            hashed = hash;
+            console.log(hash);
+        })
+    })
+})
+
+// for comparing password
+app.get("/compare",(req,res)=>{
+    bcrypt.compare('password',hashed,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+        console.log(result);
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
