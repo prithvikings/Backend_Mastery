0b966ecb001cc5e53dbe1656f416a7f8857cc922
Prithvi Raj <prithvi07raj07@gmail.com>
2025-02-02 02:59:47 +0530

    Add owner creation route and implement MongoDB connection logging

--- bagvati/Routes/ownersRouter.js
+++ bagvati/Routes/ownersRouter.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-Model');
 
 router.get('/', (req, res) => {
     res.send('Owners');
     }
 );
+
+router.post("/create", async (req, res) => {
+    let owners=await ownerModel.find();
+    if(owners.length>0){
+        return res
+        .status(400)
+        .send("You don't have permission to create more than one owner");
+    }
+    let createdOwner=await ownerModel.create({
+        fullName : req.body.fullName,
+        email : req.body.email,
+        password : req.body.password,
+    });
+    res.status(201).send(createdOwner);
+});
 
 module.exports = router;
--- bagvati/configuration/mongooseConnection.js
+++ bagvati/configuration/mongooseConnection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const dbgr= require('debug')('development:mongoose');
+
 
 mongoose
 .connect("mongodb+srv://prithvi312:fsfO7jyoofuv0jz5@cluster0.wrm4e.mongodb.net/bagvati")
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
+.then(() => dbgr('Connected to MongoDB...'))
+.catch(err => dbgr('Could not connect to MongoDB...', err));
 
 module.exports = mongoose.connection;
