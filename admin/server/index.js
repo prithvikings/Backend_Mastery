95adc5e78a54f4fda4e85dd39dfe25c31389c792
Prithvi Raj <prithvi07raj07@gmail.com>
2025-02-04 23:15:23 +0530

    Initialize server setup: create basic Express server with JSON parsing, static file serving, and a default route; add package.json with dependencies and scripts

---
+++ admin/server/index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
