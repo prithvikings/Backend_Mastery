ab29ad8930489f658073235bcdaa0d5166a44220
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2025-07-11 21:41:35 +0530

    Update script.js

--- one-liner-joke/script.js
+++ one-liner-joke/script.js
@@ -1,5 +1,6 @@
 var oneLinerJoke = require('one-liner-joke');
 
+
 /*
 The variable getRandomJoke will contain a random joke with a format:
 {"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
